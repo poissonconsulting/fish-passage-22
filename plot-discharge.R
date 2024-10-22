607a7445b7c0333e9f043935636781f86c42b600
Nicole Hill <nicole@poissonconsulting.ca>
2024-10-22 12:23:44 -0700

    Revert changes from causal modelling, which have been moved to fish-passage-22b

--- plot-discharge.R
+++ plot-discharge.R
@@ -3,14 +3,22 @@ source("header.R")
 sbf_set_sub("query")
 sbf_load_datas()
 
+discharge %<>% 
+  group_by(latitude, longitude) %>% 
+  mutate(
+    station_id = cur_group_id(),
+    station_id = factor(station_id)
+  )
+
 gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = discharge, colour = site, group = site)) +
-  ylab("Discharge (m^3/s)") +
+  geom_line(aes(x = date, y = discharge, colour = station_id, group = station_id), alpha = 0.2) +
+  ylab("Discharge") +
   xlab("Date") +
-  labs(colour = "Site") +
+  labs(colour = "Station ID") +
+  theme(legend.position = "none") +
   NULL
 
-sbf_open_window(8, 6)
+sbf_open_window(8, 10)
 sbf_print(gp)
 
 sbf_save_plot(
@@ -18,13 +26,3 @@ sbf_save_plot(
   report = FALSE,
   caption = "Mean daily discharge by date and site"
 )
-
-discharge %<>% 
-  group_by(date) %>% 
-  mutate(discharge = as.vector(scale(discharge))) %>% 
-  ungroup() %>% 
-  left_join(water_temp_site, join_by(site)) %>% 
-  ps_activate_sfc()
-
-gp <- mapview(discharge, zcol = "discharge")
-gp
